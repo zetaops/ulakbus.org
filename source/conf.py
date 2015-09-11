b02e40bbbfe3854b4a7c23f72e6633ee0d967cb0
Evren Esat Özkan <evrenesat@zetaops.io>
2015-09-11 09:22:15 +0300

    added sphinxcontrib-plantuml to sphinx config

--- source/conf.py
+++ source/conf.py
@@ -37,6 +37,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
+    'sphinxcontrib.plantuml',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
